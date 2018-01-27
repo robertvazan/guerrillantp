732ea5974d42eb4ad4431a30f34388f7d1e79b90
Robert Vazan <robvcode@gmail.com>
2018-01-27 17:54:30 +0100

    Display all information in the packet

--- NtpCommand/Program.cs
+++ NtpCommand/Program.cs
@@ -12,18 +12,29 @@ namespace NtpCommand
             var servers = args.Length > 0 ? args : new[] { "pool.ntp.org" };
             foreach (var host in servers)
             {
+                Console.WriteLine("Querying {0}...", host);
                 using (var ntp = new NtpClient(Dns.GetHostAddresses(host)[0]))
                 {
-                    ntp.Timeout = TimeSpan.FromSeconds(5);
                     var packet = ntp.Query();
-                    Console.WriteLine(host);
+                    Console.WriteLine("Received {0}B packet", packet.Bytes.Length);
                     Console.WriteLine("-------------------------------------");
-                    Console.WriteLine("Correction offset: {0}", packet.CorrectionOffset);
-                    Console.WriteLine("RTT:               {0}", packet.RoundTripTime);
+                    Console.WriteLine("Correction offset: {0:s'.'FFFFFFF}", packet.CorrectionOffset);
+                    Console.WriteLine("Round-trip time:   {0:s'.'FFFFFFF}", packet.RoundTripTime);
                     Console.WriteLine("Origin time:       {0:hh:mm:ss.fff}", packet.OriginTimestamp);
                     Console.WriteLine("Receive time:      {0:hh:mm:ss.fff}", packet.ReceiveTimestamp);
                     Console.WriteLine("Transmit time:     {0:hh:mm:ss.fff}", packet.TransmitTimestamp);
                     Console.WriteLine("Destination time:  {0:hh:mm:ss.fff}", packet.DestinationTimestamp);
+                    Console.WriteLine("Protocol version:  {0}", packet.VersionNumber);
+                    Console.WriteLine("Protocol mode:     {0}", packet.Mode);
+                    Console.WriteLine("Leap second:       {0}", packet.LeapIndicator);
+                    Console.WriteLine("Stratum:           {0}", packet.Stratum);
+                    Console.WriteLine("Reference ID:      0x{0:x}", packet.ReferenceId);
+                    Console.WriteLine("Reference time:    {0:hh:mm:ss.fff}", packet.ReferenceTimestamp);
+                    Console.WriteLine("Root delay:        {0}ms", packet.RootDelay.TotalMilliseconds);
+                    Console.WriteLine("Root dispersion:   {0}ms", packet.RootDispersion.TotalMilliseconds);
+                    Console.WriteLine("Poll interval:     2^{0}s", packet.Poll);
+                    Console.WriteLine("Precision:         2^{0}s", packet.Precision);
+                    Console.WriteLine();
                 }
             }
         }
